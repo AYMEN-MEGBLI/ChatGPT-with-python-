dcc9a8bc3409d0329cf9c817e91fb086908db7ef
Aymen Megbli <90506038+AYMEN-MEGBLI@users.noreply.github.com>
2023-01-16 11:48:29 +0100

    Update Chat GPT.py

--- Chat GPT.py
+++ Chat GPT.py
@@ -1,15 +1,16 @@
 import openai
-import time
+from time import sleep,time
 from colorama import init, Fore
+from os import remove
 init()
 #initialize module colorama
 # Define OpenAI API key (https://beta.openai.com/account/api-keys)
-openai.api_key = "YOUR API"
+openai.api_key = "your API (https://beta.openai.com/account/api-keys) "
 # Set up the model and prompt
 model_engine = "text-davinci-003"
 while 1 :
     prompt =str(input("your question\t:"))
-    start = time.time()
+    start = time()
     # Generate a response
     completion = openai.Completion.create\
         (
@@ -23,16 +24,21 @@ while 1 :
 
     reponse = completion.choices[0].text
     print('-'*100,reponse)
-    end = time.time()
+    end = time()
     print("The time used to execute this is given below  [",end - start,']')
     valid=False
     while not valid:
         condition=str(input('press ['+Fore.RED+'A'+Fore.WHITE+'] for another question | ['+Fore.RED+'S'+Fore.WHITE+'] saving response in external file | ['+Fore.RED+'L'+Fore.WHITE+'] Leaving  :   ')).upper()
         valid=condition in ['A','S','L']
     if condition=='S' :
-        with open (prompt+".txt",'w') as f:
-            f.write("-----------chatgpt-----------\n-question  :"+prompt+"\n-reponse  :"+reponse+"\n[BY Megbli Aymen]")
-        print(Fore.YELLOW,"file is maked",Fore.WHITE)
+        try :
+            x="-----------chatgpt-----------\n-question  :"+prompt+"\n-reponse  :"+reponse+"\n[BY Megbli Aymen]"
+            with open (prompt+".txt",'w') as f:
+                    f.write(x)
+            print(Fore.YELLOW,"file is created",Fore.WHITE)
+        except UnicodeEncodeError:
+            remove(prompt+".txt")
+            print(Fore.RED, "error ! file not created", Fore.WHITE)
         valid = False
         while not valid:
             condition = str(input('press ['+Fore.RED+'A'+Fore.WHITE+'] for another question | ['+Fore.RED+'L'+Fore.WHITE+'] Leaving  :   ')).upper()
@@ -40,7 +46,7 @@ while 1 :
     if condition=="L" :
         break
 
-for i in range(5,-1,-1) :
+for i in range(3,-1,-1) :
     print('\r',i,end="")
-    time.sleep(1)
+    sleep(1)
 quit()
